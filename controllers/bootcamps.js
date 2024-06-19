7ab5c24884a72d8eefe322bf16f3101fb86a68c6
Sharmaketan <ketan.sharma@iensure.in>
2024-06-19 17:46:46 +0530

    create bootcamps post method into database

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -18,8 +18,16 @@ exports.getBootcamp = (req, res, next) => {
 //@desc Create new bootcamps
 //@route POST /api/v1/bootcamps/
 //@access Private
-exports.createBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Create a bootcamps" })
+exports.createBootcamps = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body)
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    })
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
 }
 
 //@desc Update bootcamps
