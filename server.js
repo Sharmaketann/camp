1fcb94f957eab67743459954bfe67316ae040ca6
Sharmaketan <ketan.sharma@iensure.in>
2024-06-19 12:29:38 +0530

    colors package added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const dotenv = require("dotenv")
 const morgan = require("morgan")
 const connectDB = require("./config/db")
+const colors = require("colors")
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" })
@@ -26,13 +27,15 @@ const PORT = process.env.PORT || 5000
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
 
 //Handle unhandled rejections
 
 process.on("unhandledRejection", (err, promise) => {
-  console.log(`Error: ${err.message}`)
+  console.log(`Error: ${err.message}`.red)
   //Close server & exit process
   server.close(() => process.exit(1))
 })
