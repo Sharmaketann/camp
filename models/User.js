01b921139e8ba44dfa3bf9768c40dbe2142ceafd
Sharmaketan <ketan.sharma@iensure.in>
2024-07-03 15:39:09 +0530

    User Model, User Register and Password encrypt added

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email",
+    ],
+  },
+  role: {
+    type: String,
+    enum: ["user", "publisher"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+// Encrypt password using bcrypt
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next()
+  }
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
+module.exports = User = mongoose.model("user", UserSchema)
